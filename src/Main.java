b223ede8b70c203c8be0fd5d87c27fab28c46477
bt_1n3_24 <sample@bths.edu>
2023-09-21 10:16:14 -0400

    variables

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int groupNum;
+        int tipPercent;
+        double totalCost;
+        double tipCost;
+        String[] itemsOrdered;
+        String[] menuName = {"Crunchy taco", "Soft taco"};
+        double[] menuCost = {2.49, 2.99};
+
+
+
+        System.out.println("Welcome to the tip calculator");
+
+
+    }
+}
